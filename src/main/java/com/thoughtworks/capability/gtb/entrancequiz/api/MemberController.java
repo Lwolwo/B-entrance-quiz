ec3ed9add0f3479e20bdaf60dc5f14b61c0d5254
Xiaofeng Wang <xfun68@gmail.com>
2020-11-05 00:42:45 +0800

    add gtb comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/MemberController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/MemberController.java
@@ -5,13 +5,19 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import com.thoughtworks.capability.gtb.entrancequiz.service.*;
 
+//TODO GTB-完成度: * 只有查询学员和分组的实现，但设计不太正确
+
+//TODO GTB-测试: * 没有测试
+
 @CrossOrigin("http://localhost:1234")
 @RestController
 public class MemberController {
+    //TODO GTB-知识点: - 推荐使用构造函数注入
     @Autowired
     MemberService memberService;
 
     @GetMapping("/memberList")
+    //TODO GTB-知识点: - 了解一下 @ResponseStatus
     public ResponseEntity getMemberList(){
         return ResponseEntity.ok(memberService.getMemberList());
     }
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MemberService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MemberService.java
@@ -9,9 +9,11 @@ import com.thoughtworks.capability.gtb.entrancequiz.domain.*;
 @Service
 public class MemberService {
     ArrayList<Member> memberList;
+    //TODO GTB-工程实践: - copyList 是啥？？？
     ArrayList<Member> copyList;
 
     MemberService() {
+        //TODO GTB-知识点: - 尽量不用数组，多用 List
         String[] list = {"成吉思汗", "鲁班七号", "太乙真人", "钟无艳", "花木兰", "雅典娜", "芈月", "白起", "刘禅", "庄周", "马超", "刘备", "哪吒", "大乔", "蔡文姬"};
         memberList = new ArrayList<>();
         copyList = new ArrayList<>();
@@ -28,6 +30,7 @@ public class MemberService {
 
     public ArrayList<ArrayList<Member>> getSortMemberList() {
         Collections.shuffle(copyList);
+        //TODO GTB-工程实践: - 没有提取出 Group 的概念！
         ArrayList<ArrayList<Member>> list = new ArrayList<ArrayList<Member>>();
         for(int i = 0; i < 6; i++) {
             list.add(new ArrayList<Member>());
@@ -35,6 +38,7 @@ public class MemberService {
         int index = 0;
         for(int i = 0; i < 6; i++) {
             for(int j = 0; j < (memberList.size() / 6); j++) {
+                //TODO GTB-工程实践: - 变量名都叫 group 了，就是不提取一个 class 叫 Group，就差一步！
                 ArrayList<Member> group = list.get(i);
                 group.add(copyList.get(index));
                 index++;
